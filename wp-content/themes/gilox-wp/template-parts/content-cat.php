9fac9123188a62086aa655719e8df7920ec459bb
Benson Sanga <bensonsanga@hotmail.com>
2020-07-16 12:40:54 +0300

    Created by Benson Ad

--- wp-content/themes/gilox-wp/template-parts/content-cat.php
+++ wp-content/themes/gilox-wp/template-parts/content-cat.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template part for displaying page content in page.php
- *
+ * Created by Benson Ad
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package gilox-wp
