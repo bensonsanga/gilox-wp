c9721e9c21d11cb655fc9211806d9e410161c8a2
Benson Sanga <bensonsanga@hotmail.com>
2020-07-18 23:59:14 +0300

    ddd

--- wp-content/themes/gilox-wp/index.php
+++ wp-content/themes/gilox-wp/index.php
@@ -34,7 +34,7 @@ get_header();
 
 				/*
 				 * Include the Post-Type-specific template for    the content.
-				 * If you want to overrideghdhshsbdb this in a child theme, then include a file
+				 * If you want to overrideg      hdhshsbdb this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/content', get_post_type() );
