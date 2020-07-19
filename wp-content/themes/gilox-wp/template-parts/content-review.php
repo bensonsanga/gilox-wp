e8c4734f30645c602368e8cc0cad7c32dbe8c48c
Benson Sanga <bensonsanga@hotmail.com>
2020-07-19 16:27:33 +0300

    deleteed the meta

--- wp-content/themes/gilox-wp/template-parts/content-review.php
+++ wp-content/themes/gilox-wp/template-parts/content-review.php
@@ -30,7 +30,7 @@
                     
                     By <?php the_author(); ?><br />
                     
-                </div><!-- .entry-meta -->
+                </div>
             <?php endif; ?>
 
         
@@ -38,9 +38,9 @@
                     echo '<p class="card-legend__description">' . get_the_excerpt() . '</p>';
                 ?>
             
-        </div><!-- .entry-content -->
+        </div>
 
-    </article><!-- #post-<?php the_ID(); ?> -->
+    </article>
 </li>
 
 
@@ -49,18 +49,4 @@
 
 
 
-<!--
-
-		<article class="card-story" itemscope itemtype="https://schema.org/CreativeWork">
-			<a href="greek-mythology/articles/article1.html" class="card-story__media" itemprop="image" itemscope itemtype="https://schema.org/ImageObject"><img alt="Duduke video" title="News and Entertainment Gists" class="card-story__image" src="<?php bloginfo('template_directory');?>/images/Avatars/1x/avatar.jpg">
-            </a>
-            <div class="card-story__text">
-                <h4 class="card-story__title" itemprop="name">
-                    <a href="greek-mythology/articles/article1.html" class="card-story__link" itemprop="url">Latest Article 1</a>
-                </h4>
-                <div class="card-story__author" itemprop="author" itemscope itemtype="https://schema.org/Person">
-                    <a href="#" class="card-story__author-name" itemprop="url">          <span itemprop="name">Simisola</span></a>
-                </div>
-                <p class="card-story__description" itemprop="description">This music video was release by Simisola to celebrate her unborn baby. A challenge has been initiated on Twitter regarding this via the hashtag #dudukechallenge.</p>
-            </div>
-      </article><!-- #post-<?php the_ID(); ?> -->
+<?php the_ID(); ?>
