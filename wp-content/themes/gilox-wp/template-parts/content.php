c45f2b8c661caa5f5dc5759e0ee4fd03123bae52
Benson Sanga <bensonsanga@hotmail.com>
2020-07-19 16:26:38 +0300

    Simple commit

--- wp-content/themes/gilox-wp/template-parts/content.php
+++ wp-content/themes/gilox-wp/template-parts/content.php
@@ -75,12 +75,5 @@
 			<a href="greek-mythology/articles/article1.html" class="card-story__media" itemprop="image" itemscope itemtype="https://schema.org/ImageObject"><img alt="Duduke video" title="News and Entertainment Gists" class="card-story__image" src="<?php bloginfo('template_directory');?>/images/Avatars/1x/avatar.jpg">
             </a>
             <div class="card-story__text">
-                <h4 class="card-story__title" itemprop="name">
-                    <a href="greek-mythology/articles/article1.html" class="card-story__link" itemprop="url">Latest Article 1</a>
-                </h4>
-                <div class="card-story__author" itemprop="author" itemscope itemtype="https://schema.org/Person">
-                    <a href="#" class="card-story__author-name" itemprop="url">          <span itemprop="name">Simisola</span></a>
-                </div>
-                <p class="card-story__description" itemprop="description">This music video was release by Simisola to celebrate her unborn baby. A challenge has been initiated on Twitter regarding this via the hashtag #dudukechallenge.</p>
-            </div>
+                           </div>
       </article><!-- #post-<?php the_ID(); ?> -->
