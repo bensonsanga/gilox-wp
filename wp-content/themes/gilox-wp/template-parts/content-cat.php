c5cee3a7a4d468fb9fc6313043c69d03fef2d15b
Benson Sanga <bensonsanga@hotmail.com>
2020-07-29 19:42:17 +0300

    pop

--- wp-content/themes/gilox-wp/template-parts/content-cat.php
+++ wp-content/themes/gilox-wp/template-parts/content-cat.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying page content in page.php
+ * Template part for     displaying page content in page.php
  * Created by Benson Ad
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
