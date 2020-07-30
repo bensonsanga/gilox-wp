5a21870d1ed8bcdd05af82cf33171f5fabcc97b5
Benson Sanga <bensonsanga@hotmail.com>
2020-07-30 19:27:04 +0300

    added the changes

--- wp-content/themes/gilox-wp/template-parts/content-cat.php
+++ wp-content/themes/gilox-wp/template-parts/content-cat.php
@@ -11,53 +11,18 @@
 <div class="article-grid">
 <a href="<?php echo get_permalink();?>" class="article-card article-card--large-2x">
   <div class="article-card__media">
-          <img alt="Just give it a sec" title="Just give it a sec" class="article-card__image lazyload" loading="lazy" data-sizes="(min-width: 1920px) 736px, (min-width: 1280px) 42.5vw, (min-width: 640px) 50vw, 100vw" data-srcset="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=320&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=360&amp;ar=3:2&amp;fit=crop&amp;auto=format 360w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=375&amp;ar=3:2&amp;fit=crop&amp;auto=format 375w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=480&amp;ar=3:2&amp;fit=crop&amp;auto=format 480w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=540&amp;ar=3:2&amp;fit=crop&amp;auto=format 540w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=576&amp;ar=3:2&amp;fit=crop&amp;auto=format 576w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=640&amp;ar=3:2&amp;fit=crop&amp;auto=format 640w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=720&amp;ar=3:2&amp;fit=crop&amp;auto=format 720w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=750&amp;ar=3:2&amp;fit=crop&amp;auto=format 750w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=768&amp;ar=3:2&amp;fit=crop&amp;auto=format 768w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=800&amp;ar=3:2&amp;fit=crop&amp;auto=format 800w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=960&amp;ar=3:2&amp;fit=crop&amp;auto=format 960w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1024&amp;ar=3:2&amp;fit=crop&amp;auto=format 1024w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1080&amp;ar=3:2&amp;fit=crop&amp;auto=format 1080w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1160&amp;ar=3:2&amp;fit=crop&amp;auto=format 1160w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1280&amp;ar=3:2&amp;fit=crop&amp;auto=format 1280w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1360&amp;ar=3:2&amp;fit=crop&amp;auto=format 1360w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1440&amp;ar=3:2&amp;fit=crop&amp;auto=format 1440w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1472&amp;ar=3:2&amp;fit=crop&amp;auto=format 1472w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1536&amp;ar=3:2&amp;fit=crop&amp;auto=format 1536w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1600&amp;ar=3:2&amp;fit=crop&amp;auto=format 1600w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1680&amp;ar=3:2&amp;fit=crop&amp;auto=format 1680w" data-src="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=640&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w" src="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=640&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w" sizes="(min-width: 1920px) 736px, (min-width: 1280px) 42.5vw, (min-width: 640px) 50vw, 100vw" srcset="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=320&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=360&amp;ar=3:2&amp;fit=crop&amp;auto=format 360w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=375&amp;ar=3:2&amp;fit=crop&amp;auto=format 375w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=480&amp;ar=3:2&amp;fit=crop&amp;auto=format 480w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=540&amp;ar=3:2&amp;fit=crop&amp;auto=format 540w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=576&amp;ar=3:2&amp;fit=crop&amp;auto=format 576w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=640&amp;ar=3:2&amp;fit=crop&amp;auto=format 640w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=720&amp;ar=3:2&amp;fit=crop&amp;auto=format 720w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=750&amp;ar=3:2&amp;fit=crop&amp;auto=format 750w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=768&amp;ar=3:2&amp;fit=crop&amp;auto=format 768w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=800&amp;ar=3:2&amp;fit=crop&amp;auto=format 800w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=960&amp;ar=3:2&amp;fit=crop&amp;auto=format 960w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1024&amp;ar=3:2&amp;fit=crop&amp;auto=format 1024w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1080&amp;ar=3:2&amp;fit=crop&amp;auto=format 1080w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1160&amp;ar=3:2&amp;fit=crop&amp;auto=format 1160w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1280&amp;ar=3:2&amp;fit=crop&amp;auto=format 1280w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1360&amp;ar=3:2&amp;fit=crop&amp;auto=format 1360w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1440&amp;ar=3:2&amp;fit=crop&amp;auto=format 1440w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1472&amp;ar=3:2&amp;fit=crop&amp;auto=format 1472w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1536&amp;ar=3:2&amp;fit=crop&amp;auto=format 1536w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1600&amp;ar=3:2&amp;fit=crop&amp;auto=format 1600w,
-                       <?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1680&amp;ar=3:2&amp;fit=crop&amp;auto=format 1680w">
+          <img alt="Just give it a sec" title="Just give it a sec" class="article-card__image lazyload" loading="lazy" 
+          data-sizes="(min-width: 1920px) 736px, (min-width: 1280px) 42.5vw, (min-width: 640px) 50vw, 100vw" 
+          data-srcset="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=1680&amp;ar=3:2&amp;fit=crop&amp;auto=format 1680w" 
+            data-src="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=640&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w" 
+            src="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=640&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w" 
+            sizes="(min-width: 1920px) 736px, (min-width: 1280px) 42.5vw, (min-width: 640px) 50vw, 100vw" 
+            srcset="<?php echo get_the_post_thumbnail_url();?>?q=75&amp;w=320&amp;ar=3:2&amp;fit=crop&amp;auto=format 320w,
+">
   </div>
   <div class="article-card__text">
     <!-- <h5 class="article-card__subtitle"><?php echo get_the_category(); ?></h5> -->
-    <h3 class="article-card__title"><?php echo the_title(); ?></h3>
+    <h4 class="article-item__title"><?php echo the_title(); ?></h4>
     <p class="article-card__description"><?php echo get_the_excerpt(); ?></p>
     <div class="article-byline">
       <div class="article-byline__icon">
